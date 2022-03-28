a006e3619dbb108d1e916a9153d30497560c64aa
jtschuwirth <jtschuwirth@uc.cl>
2022-03-28 11:09:02 -0300

    progress

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,24 +1,89 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+import { StringUtils } from "./libraries/StringUtils.sol";
+import {Base64} from "./libraries/Base64.sol";
+
 import "hardhat/console.sol";
 
-contract Domains {
-  mapping(string => address) public domains;
-  
-  // Checkout our new mapping! This will store values
-  mapping(string => string) public records;
+contract Domains is ERC721URIStorage {
 
-  constructor() {
-      console.log("Yo yo, I am a contract and I am smart");
-  }
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-  function register(string calldata name) public {
-      // Check that the name is unregistered (explained in notes)
-      require(domains[name] == address(0));
-      domains[name] = msg.sender;
-      console.log("%s has registered a domain!", msg.sender);
-  }
+    string svgPartOne = '';
+    string svgPartTwo = '';
+
+    string public tld;
+
+    mapping(string => address) public domains;
+    mapping(string => string) public records;
+
+    constructor(string memory _tld) payable ERC721("Patagonian Name Service", "PNS") {
+        tld = _tld;
+        console.log("%s name service deployed", _tld);
+    }
+
+    function price(string calldata name) public pure returns(uint) {
+        uint len = StringUtils.strlen(name);
+        require(len > 0);
+        if (len == 3) {
+            return 5 * 10**17; 
+        } else if (len == 4) {
+            return 3 * 10**17;
+        } else {
+            return 1 * 10**17;
+        }
+    }
+
+    function register(string calldata name) public payable {
+        require(domains[name] == address(0));
+
+        uint256 _price = price(name);
+        require(msg.value >= _price, "Not enough Matic paid");
+		
+		    // Combine the name passed into the function  with the TLD
+        string memory _name = string(abi.encodePacked(name, ".", tld));
+		    // Create the SVG (image) for the NFT with the name
+        string memory finalSvg = string(abi.encodePacked(svgPartOne, _name, svgPartTwo));
+        uint256 newRecordId = _tokenIds.current();
+  	    uint256 length = StringUtils.strlen(name);
+		    string memory strLen = Strings.toString(length);
+
+        console.log("Registering %s.%s on the contract with tokenID %d", name, tld, newRecordId);
+
+		    // Create the JSON metadata of our NFT. We do this by combining strings and encoding as base64
+        string memory json = Base64.encode(
+        bytes(
+            string(
+            abi.encodePacked(
+                '{"name": "',
+                _name,
+                '", "description": "A domain on the Patagonian name service", "image": "data:image/svg+xml;base64,',
+                Base64.encode(bytes(finalSvg)),
+                '","length":"',
+                strLen,
+                '"}'
+            )
+            )
+        )
+        );
+
+        string memory finalTokenUri = string( abi.encodePacked("data:application/json;base64,", json));
+
+		    console.log("\n--------------------------------------------------------");
+	    console.log("Final tokenURI", finalTokenUri);
+	    console.log("--------------------------------------------------------\n");
+
+        _safeMint(msg.sender, newRecordId);
+        _setTokenURI(newRecordId, finalTokenUri);
+        domains[name] = msg.sender;
+
+        _tokenIds.increment();
+    }
 
   function getAddress(string calldata name) public view returns (address) {
       // Check that the owner is the transaction sender
--- scripts/run.js
+++ scripts/run.js
@@ -1,23 +1,31 @@
 const main = async () => {
     // The first return is the deployer, the second is a random account
     const [owner, randomPerson] = await hre.ethers.getSigners();
+
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
+    const domainContract = await domainContractFactory.deploy("ptg");
     await domainContract.deployed();
+
     console.log("Contract deployed to:", domainContract.address);
     console.log("Contract deployed by:", owner.address);
   
-    let txn = await domainContract.register("Neca");
+    let txn = await domainContract.register("Neca",  {value: hre.ethers.utils.parseEther('0.3')});
     await txn.wait();
   
     const domainAddress = await domainContract.getAddress("Neca");
     console.log("Owner of domain Neca:", domainAddress);
 
-    txn = await domainContract.connect(randomPerson).register("Random");
+    let balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+
+    txn = await domainContract.connect(randomPerson).register("Random",  {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
 
     const domainAddress2 = await domainContract.getAddress("Random");
     console.log("Owner of domain Random:", domainAddress2);
+
+    balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 }
   
   const runMain = async () => {
